0050778a969cfb6290afba8361849f3097d13707
Sunny <send2kiran.n@gmail.com>
2025-10-24 23:39:09 +1100

    updated system.

--- Alaris.Quantlib/obj/Debug/net9.0/Alaris.Quantlib.AssemblyInfo.cs
+++ Alaris.Quantlib/obj/Debug/net9.0/Alaris.Quantlib.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCopyrightAttribute("Copyright © 2025")]
 [assembly: System.Reflection.AssemblyDescriptionAttribute("QuantLib C# bindings via SWIG for the Alaris quantitative finance system")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b6437a3b3fee19d053746bf4f072ac79616a2e48")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e6d8a7edde18fcccdcaedf0bfc88c6694f40cce3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Alaris.Quantlib")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Alaris.Quantlib")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Alaris.Strategy/obj/Debug/net9.0/Alaris.Strategy.AssemblyInfo.cs
+++ Alaris.Strategy/obj/Debug/net9.0/Alaris.Strategy.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyDescriptionAttribute("Earnings volatility calendar spread trading strategies based on academic research")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b6437a3b3fee19d053746bf4f072ac79616a2e48")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e6d8a7edde18fcccdcaedf0bfc88c6694f40cce3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Alaris.Strategy")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Alaris.Strategy")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
